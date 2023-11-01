37f4b3d00fa1f25949a18619696252dbabfd9dc7
Ben Lambert <114227087+BenLambertTopG@users.noreply.github.com>
2023-11-01 10:34:35 +0000

    Original Upload
    Original Upload

---
+++ Blast Off/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+main()
+{
+
+    for (int i{5}; i > 0; --i)
+        std::cout << i << "!" << std::endl;
+    std::cout << "Thunderbirds Are GO!!" << std::endl;
+
+    return 0;
+}
